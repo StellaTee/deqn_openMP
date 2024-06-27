b65638dc029c2e1cc7a8d1c89675541cba508e43
StellaTee <ztee0004@student.monash.edu>
2024-06-28 00:05:55 +0100

    upload all the files

---
+++ deqn/src/main.C
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <omp.h>
+
+#include "InputFile.h"
+#include "Driver.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        std::cerr << "Usage: deqn <filename>" << std::endl;
+        exit(1);
+    }
+
+    // set the number of threads to observe the performance
+    omp_set_num_threads(16);
+
+    double start_time = omp_get_wtime(); 
+    const char* filename = argv[1];
+    InputFile input(filename);
+
+    std::string problem_name(filename);
+
+    int len = problem_name.length();
+
+    if(problem_name.substr(len - 3, 3) == ".in")
+        problem_name = problem_name.substr(0, len-3);
+
+    // Strip out leading path
+    size_t last_sep = problem_name.find_last_of("/");
+
+    if (last_sep != std::string::npos) {
+        last_sep = last_sep + 1;
+    } else {
+        last_sep = 0;
+    }
+
+    problem_name = problem_name.substr(last_sep, problem_name.size());
+    #pragma omp parallel
+    {
+        # pragma omp master
+        {
+            std::cout << "Number of threads: " << omp_get_num_threads() << std::endl;
+        }
+    }
+
+    Driver driver(&input, problem_name);
+
+    driver.run();
+    
+    double end_time = omp_get_wtime(); // Get the end time
+
+    printf("(Main) Runtime: %f seconds\n", end_time - start_time);
+
+    return 0;
+}
